8f9b22fdae5e3f002e0c53aed5c0244393a5fbbf
Gud <toby.hayter10@gmail.com>
2026-01-30 17:59:55 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,24 @@
+AltTexture({
+    key = "blinds",
+    set = "Blind",
+    path = "blind.png",
+    original_sheet = true,
+    frames = 21,
+    display_pos = "bl_final_leaf",
+    loc_txt = {
+        name = "Tag blinds"
+    }
+})
+
+TexturePack{
+    key = "blind",
+    textures = {
+        "blindtag_blinds"
+    },
+    loc_txt = {
+        name = "Tag blinds",
+        text = {
+            "Turns the blinds into tags",
+        }
+    }
+}
